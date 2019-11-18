349961f85d8e198fdff170f3f00fd95ceb97a852
vade <vade@vade.info>
2019-11-18 13:14:11 -0500

    re-add our other groups now that we have label pre-filtering

--- Utilities/auto_labeler_color_key.py
+++ Utilities/auto_labeler_color_key.py
@@ -45,17 +45,15 @@ not_applicable_concepts = [
 "color_saturation",
 "color_theory",
 "color_tones",
-
-# NOTE: We had WAY too many images for the NA - which made the model way less confident for any actual green / blue screen images
-# "shot_angle",
-# "shot_focus",
-# "shot_framing",
-# "shot_level",
-# "shot_lighting",
-# "shot_location",
-# "shot_subject",
-# "shot_timeofday",
-# "shot_type",
+"shot_angle",
+"shot_focus",
+"shot_framing",
+"shot_level",
+"shot_lighting",
+"shot_location",
+"shot_subject",
+"shot_timeofday",
+"shot_type",
 "texture",
 ]
 
