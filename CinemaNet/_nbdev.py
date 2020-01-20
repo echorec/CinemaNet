7324e6cfae195d39f679af5dc9e493c48783d8f7
rsomani95 <rsomani95@gmail.com>
2020-01-21 01:51:42 +0530

    nbdev exports

--- CinemaNet/_nbdev.py
+++ CinemaNet/_nbdev.py
@@ -2,29 +2,29 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"create_head": "fastai_custom_head.ipynb",
-         "create_cnn_model": "fastai_custom_head.ipynb",
-         "cnn_learner": "fastai_custom_head.ipynb",
-         "xtra_tfms": "mobilenet_v2_basic.ipynb",
-         "get_tfms": "mobilenet_v2_basic.ipynb",
-         "to_film_ar": "mobilenet_v2_basic.ipynb",
-         "get_data": "mobilenet_v2_basic.ipynb",
-         "get_callbacks": "mobilenet_v2_basic.ipynb",
-         "cb_estop": "mobilenet_v2_basic.ipynb",
-         "metrics": "mobilenet_v2_basic.ipynb",
-         "cb_wandb": "mobilenet_v2_basic.ipynb",
-         "torch_to_onnx": "pytorch_to_coreml.ipynb",
-         "onnx_to_coreml": "pytorch_to_coreml.ipynb",
-         "args": "pytorch_to_coreml.ipynb",
-         "add_scaler": "pytorch_to_coreml.ipynb",
-         "save_coreml_model": "pytorch_to_coreml.ipynb",
-         "torch_to_coreml": "pytorch_to_coreml.ipynb",
-         "WandbCallback": "wandb_callback.ipynb"}
+index = {"create_head": "01_fastai_custom_head.ipynb",
+         "create_cnn_model": "01_fastai_custom_head.ipynb",
+         "cnn_learner": "01_fastai_custom_head.ipynb",
+         "WandbCallback": "02_wandb_callback.ipynb",
+         "torch_to_onnx": "03_pytorch_to_coreml.ipynb",
+         "onnx_to_coreml": "03_pytorch_to_coreml.ipynb",
+         "args": "03_pytorch_to_coreml.ipynb",
+         "add_scaler": "03_pytorch_to_coreml.ipynb",
+         "save_coreml_model": "03_pytorch_to_coreml.ipynb",
+         "torch_to_coreml": "03_pytorch_to_coreml.ipynb",
+         "xtra_tfms": "04_mobilenet_v2_basic.ipynb",
+         "get_tfms": "04_mobilenet_v2_basic.ipynb",
+         "to_film_ar": "04_mobilenet_v2_basic.ipynb",
+         "get_data": "04_mobilenet_v2_basic.ipynb",
+         "get_callbacks": "04_mobilenet_v2_basic.ipynb",
+         "cb_estop": "04_mobilenet_v2_basic.ipynb",
+         "metrics": "04_mobilenet_v2_basic.ipynb",
+         "cb_wandb": "04_mobilenet_v2_basic.ipynb"}
 
 modules = ["custom_head.py",
-           "train_utils.py",
+           "wandb.py",
            "convert.py",
-           "wandb.py"]
+           "train_utils.py"]
 
 doc_url = "https://Synopsis.github.io/CinemaNet/"
 
