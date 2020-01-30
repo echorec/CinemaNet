d0e484b9e891a57a462591c39b270b6bfb6d6543
rsomani95 <rsomani95@gmail.com>
2020-01-30 17:19:02 +0530

    bugfixes; downloading with `wget` instead of `urllib`

--- Utilities/FilmGrabDataSetDownloader.py
+++ Utilities/FilmGrabDataSetDownloader.py
@@ -5,6 +5,7 @@ import os
 from multiprocessing import Pool
 import unicodedata
 import re
+import wget
 
 parser = argparse.ArgumentParser(description='Download images from Film-grab website')
 parser.add_argument('-o', '--output', type=str, help="destination for downloaded images", default="./film-grab", required=False)
@@ -41,61 +42,56 @@ def slugify(value, allow_unicode=False):
 
 def get_filmgrab_movie_urls():
 
-	list_of_movies_url = BASE_URL + "movies-a-z/"
+    list_of_movies_url = BASE_URL + "movies-a-z/"
 
-	movies_a_z_html = urlopen(list_of_movies_url).read()
+    movies_a_z_html = urlopen(list_of_movies_url).read()
 
-	soup = BeautifulSoup(movies_a_z_html, "lxml")
+    soup = BeautifulSoup(movies_a_z_html, "lxml")
 
-	#movies are part of a li class "listing-item" within a ul "display-posts-listing"
-	movie_items = soup.find("ul", "display-posts-listing")
+    #movies are part of a li class "listing-item" within a ul "display-posts-listing"
+    movie_items = soup.find("ul", "display-posts-listing")
 
-	movie_urls = [movie.a["href"] for movie in movie_items.findAll("li")]
-	return movie_urls
+    movie_urls = [movie.a["href"] for movie in movie_items.findAll("li")]
+    return movie_urls
 
 
 def get_filmgrab_movie_images(movie_url):
-	movie_html = urlopen(movie_url).read()
-	soup = BeautifulSoup(movie_html, "lxml")
+    movie_html = urlopen(movie_url).read()
+    soup = BeautifulSoup(movie_html, "lxml")
 
-	#make a folder with the title of the movies name: (and remove the - FILMGRAB [ ] in the name)
-	# NOTE THIS IS A UNICODE - SO WE SHOULD BE USING PYTHON 3
-	film_grab_title_split = " – FILMGRAB"
-	movie_title = soup.title.string.split(film_grab_title_split, 1)[0]
+    #make a folder with the title of the movies name: (and remove the - FILMGRAB [ ] in the name)
+    # NOTE THIS IS A UNICODE - SO WE SHOULD BE USING PYTHON 3
+    film_grab_title_split = " – FILMGRAB"
+    movie_title = soup.title.string.split(film_grab_title_split, 1)[0]
 
-	movie_title = slugify(movie_title)
+    movie_title = slugify(movie_title)
 
-	print("Downloading - " + movie_title)
+    movie_download_folder = os.path.join(download_path, movie_title)
+    try:
+        os.stat(movie_download_folder)
+    except:
+        os.mkdir(movie_download_folder)  
 
-	movie_download_folder = os.path.join(download_path, movie_title)
-	try:
-	    os.stat(movie_download_folder)
-	except:
-	    os.mkdir(movie_download_folder)  
+    # all movie images look like they are in divs 
+    # named "bwg-item"
+    # within a main div named "bwg-container-0"
+    
+    movie_images_container_div = soup.find("div", "bwg-container-0")
+    if movie_images_container_div is None:
+        print(f"Skipping {movie_title} as it doesn't have any data")
+        pass
+    else:
+        print(f"\nDownloading {movie_title}")
+        movie_image_urls=[]
+        movie_images_container_div.findAll("div", "bwg-item")
+        movie_image_urls = [image_container_div.a["href"] for image_container_div in movie_images_container_div.findAll("div", "bwg-item")]
 
-	# all movie images look like they are in divs 
-	# named "bwg-item"
-	# within a main div named "bwg-container-0"
-
-	movie_images_container_div = soup.find("div", "bwg-container-0")
-	movie_image_urls = [image_container_div.a["href"] for image_container_div in movie_images_container_div.findAll("div", "bwg-item")]
-
-	image_count = 0
-	for image_url in movie_image_urls:
-
-		image_open_url = urlopen(image_url)
-		image_open_url_headers = image_open_url.headers
-
-		contentType = image_open_url_headers.get('content-type')
-
-		image_name = movie_title + "_" + str(image_count) + "." + contentType.replace("image/", "")
-		image_data = image_open_url.read()
-
-		f = open(os.path.join(movie_download_folder, image_name), 'wb')
-		f.write(image_data)
-		f.close()
-
-		image_count = image_count + 1
+        image_count = 0
+        for image_url in movie_image_urls:
+            ext=image_url.rsplit('.')[-1]
+            wget.download(image_url, out=f"{movie_title}_{str(image_count+1)}.{ext}")
+            image_count = image_count + 1
+        print(f"\n--- Downloaded {image_count} images for {movie_title}")
 
 # download our images
 movie_urls = get_filmgrab_movie_urls()
